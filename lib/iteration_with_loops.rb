a140f796fa48448c1f1188732469de1c1a235f63
Elias Campos <ecampose76@gmail.com>
2020-11-12 21:58:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ while row_index < src.count do
   
   while element_index < src[row_index].count do 
     if src[row_index][element_index].is_a? String
-      strings += src[row_index][element_index].to_string
+      strings += src[row_index][element_index].String
     end
     
     element_index += 1 
